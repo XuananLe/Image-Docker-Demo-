3179d28898a901326b8c319f6940295fb560d631
xuananle <lexuanan18102004@gmail.com>
2023-09-05 15:28:46 +0700

    adding more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is easy 🐳' }) 
+    res.send('Hello World!')
 );
 
 const port = process.env.PORT || 8080;
